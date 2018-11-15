8f38d5da57c4b22f9a58855895089767701f7216
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-11-15 18:36:57 -0500

    Write parseFile function to refactor main
    This also has some comments about me not understanding things. I still
    don't!

--- main.hs
+++ main.hs
@@ -5,9 +5,18 @@ import System.Environment
 import Parser
 import Resolve
 
+-- I'm not sure what the linter is complaining about here. Something about
+-- functors? I don't really know what those are yet.
+-- (I think that it's just a way to map and raise back to the monad. Something
+-- like `f <$> g == g >>= (return . f)`.
+-- Oh yeah! Monads support fmap, which is really what we're doing here. <$> is
+-- just an infix synonym for fmap.
+parseFile :: String -> IO [Term]
+parseFile path = fmap (toCNF . readLines) $ readFile path
+    where toCNF = splitCons . map cnf
+
 main = do
     (path:_) <- getArgs
-    contents <- readFile path
     putStrLn $ "Conjunctive normal form of " ++ path ++ ":"
     -- Why do this fancy mapM_ stuff here?
     -- Because main has type IO () (for this function).
@@ -15,4 +24,4 @@ main = do
     -- We want to do a bunch of stuff to a list and not care about the results,
     -- which is precisely what mapM_ does. (cf. mapM, which would return IO
     -- [()].
-    mapM_ (print . cnf) $ readLines contents
+    parseFile path >>= mapM_ print
