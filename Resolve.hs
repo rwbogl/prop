04616289af1ba05783b796cfbb96b48e2c9fb952
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-11-16 01:51:18 -0500

    Add first attempt at resolution rule
    Plus a note describing my pleasure at using monads!

--- Resolve.hs
+++ Resolve.hs
@@ -1,6 +1,8 @@
 module Resolve where
 
 import Parser
+import Data.List
+import Data.Maybe
 
 -- Translate a term into conjunctive normal form.
 cnf :: Term -> Term
@@ -25,6 +27,7 @@ neg (Var a) = Neg (Var a)
 neg (Dis x y) = Con (neg x) (neg y)
 neg (Con x y) = Dis (neg x) (neg y)
 neg (Query x) = Query $ neg x
+neg (Neg t) = t
 
 {-
    Can `query` be proven from a given list of clauses?
@@ -37,3 +40,22 @@ neg (Query x) = Query $ neg x
 -}
 -- resolve :: Term -> [Term] -> Bool
 -- resolve query clauses = (not . sat) $ map cnf (neg query : clauses)
+
+-- Flatten an n-ary disjunction into a list of literals.
+flattenDis :: Term -> [Term]
+flattenDis (Dis x y) = flattenDis x ++ flattenDis y
+flattenDis other = [other]
+
+-- Concat a list of terms into a single disjunction with an Empty.
+deepenDis :: [Term] -> Term
+deepenDis = foldr Dis Empty
+
+-- Oh shit, we got some monads up in here.
+-- First time I've used them "in the wild."
+resolve :: Term -> Term -> Maybe Term
+resolve left@(Dis _ _) right@(Dis _ _) = do
+    x <- find ((`elem` disRight) . neg) disLeft
+    let resolvant = (disLeft \\ [x]) `union` (disRight \\ [neg x]) in
+        return $ deepenDis resolvant
+            where disLeft = nub $ flattenDis left
+                  disRight = nub $ flattenDis right
