aafa4fa423674119bdf6a463f6ec8d65514fe44e
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-11-16 16:30:16 -0500

    Change >>= to do-notation

--- main.hs
+++ main.hs
@@ -19,10 +19,11 @@ vl = flattenDis v
 main = do
     (path:_) <- getArgs
     putStrLn $ "Conjunctive normal form of " ++ path ++ ":"
+    clauses <- parseFiletoCNF path
     -- Why do this fancy mapM_ stuff here?
     -- Because main has type IO () (for this function).
     -- Therefore, we have to ensure that our last expression returns IO ().
     -- We want to do a bunch of stuff to a list and not care about the results,
     -- which is precisely what mapM_ does. (cf. mapM, which would return IO
     -- [()].
-    parseFiletoCNF path >>= mapM_ print
+    mapM_ print clauses
