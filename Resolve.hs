fa74dfe8c7f88de29273c83d09f3e6533508fd26
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-11-26 12:40:16 -0500

    Make sat algorithm use BFS to return disproof
    Since this is a BFS, it also returns the shortest disproof possible. I
    rolled my own list-based queue implementation from looking at:
    https://lettier.github.io/posts/2016-04-29-breadth-first-search-in-haskell.html
    
    I could probably switch this over to Data.Sequence without much trouble
    if it seems worth it. A project for a later commit!

--- Resolve.hs
+++ Resolve.hs
@@ -19,7 +19,7 @@ neg (Neg t) = t
 
 {-| Check if a CNF list of clauses entail a query. -}
 clausesEntail :: CNF -> Term -> Bool
-clausesEntail clauses (Query x) = not . sat $ negated ++ clauses
+clausesEntail clauses (Query x) = isJust . sat $ negated ++ clauses
     where negated = clauseToCNF $ neg x
 
 {-| Translate a term into a CNF list. -}
@@ -52,9 +52,20 @@ cnf (Dis (x `Con` y) (Var a)) = cnf $ Dis (Var a) (x `Con` y)
 cnf (Query t) = Query $ cnf t
 cnf oth = oth
 
-{-| Check if a CNF list of clauses are satisfiable. -}
-sat :: CNF -> Bool
-sat clauses = sat' clauses []
+data QueueItem = QueueItem { left :: DisList
+                           , right :: DisList
+                           , res :: DisList
+                           , prev :: QueueItem
+                           }
+               | Empty
+               deriving Show
+
+type Queue = [QueueItem]
+
+{-| Try to prove that a CNF list of clauses is satisfiable. If so, then return
+   the proof as a QueueItem. Otherwise, return Nothing. -}
+sat :: CNF -> Maybe QueueItem
+sat clauses = sat' clauses [Empty] []
 
 {- Resolution algorithm:
     1. If possible, choose any two clauses with complementary literals. If
@@ -64,19 +75,32 @@ sat clauses = sat' clauses []
     4. If it's not empty, then go back to step 1.
 -}
 
-sat' :: CNF -> [(DisList, DisList)] -> Bool
-sat' clauses seen
-  | null resolvants = True
-  | any null resolvants = False
-  | otherwise = sat' (clauses ++ resolvants) (seen ++ newSeen)
-    where resolvePairs = [((x, y), fromJust r) | x <- clauses, y <- clauses,
-                                                    (x, y) `notElem` seen,
-                                                    let r = resolve x y,
-                                                    isJust r]
-          (newSeen, resolvants) = unzip resolvePairs
+sat' :: CNF -> Queue -> [(DisList, DisList)] -> Maybe QueueItem
+sat' init [] _ = Nothing
+sat' init (Empty:rest) _ = sat' init (packResolvants (findResolvants init) Empty) []
+sat' init (head:rest) seen
+  | null $ res head = Just head -- Derived the empty clause.
+  | otherwise = sat' init queue seen' -- Add to the queue and move on.
+  where seen' = (right head, left head):(left head, right head):seen
+        newQueries = res head : path head
+        neighbors = findResolvants (init ++ newQueries)
+        newNeighbors = filter ((`notElem` seen) . fst) neighbors
+        enqueue = packResolvants newNeighbors head
+        queue = rest ++ enqueue
 
-{-| Try to resolve two disjunctions of literals. -}
-resolve :: DisList -> DisList -> Maybe DisList
-resolve left right = do
-    x <- find (\x -> neg x `elem` right) left
-    return $ (left \\ [x]) `union` (right \\ [neg x])
+path :: QueueItem -> [DisList]
+path Empty = []
+path item = res item : path (prev item)
+
+packResolvants :: [((DisList, DisList), [DisList])] -> QueueItem -> [QueueItem]
+packResolvants xs prev = concat [[QueueItem x y r prev | r <- rs] | ((x, y), rs) <- xs]
+
+findResolvants :: CNF -> [((DisList, DisList), [DisList])]
+findResolvants clauses = [((x, y), fullResolve x y) | x <- clauses, y <- clauses]
+
+{-| Fully resolve two CNF terms. That is, return a list of all possible
+   resolvants. -}
+fullResolve :: DisList -> DisList -> [DisList]
+fullResolve left right = [resolve left right x | x <- markedLiterals]
+    where markedLiterals = filter (\x -> neg x `elem` right) left
+          resolve left right x = (left \\ [x]) `union` (right \\ [neg x])
