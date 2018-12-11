a20ec376733493b76dc60e3a204b56693c74384a
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-12-11 17:03:26 -0500

    Remove "Done" and superfluous "Next Up" comments
    This is what commit messages are for!

--- main.hs
+++ main.hs
@@ -17,32 +17,13 @@ isQuery _ = False
 
     Next up:
 
-        2. Improve our resolution algorithm implementation. Right now we just
-        try every possible combination, which is easy to write, but terribly
-        inefficient. We should do a proper search algorithm.
-
-        Note: After finishing 3, I think it's cool to find the shortest
-        possible proof. We could look at more efficient SAT algorithms, but
-        they won't find the shortest one. We'll need to stress test the current
-        algorithm and see when it becomes unbearably slow.
-
-        4. Implement a REPL.
-
-        5. Improve the data structure design. I have plans to implement a few
+        Improve the data structure design. I have plans to implement a few
         different proof algorithms, and the current design is awful. There are
         `DisList`s and `CNF`s floating around everywhere.
 
         I think that some of this could be cleared up by using `newtype` rather
         than `type` to define the `DisList` and `CNF` types, but I might have
         to experiment a bit and find out.
-
-    Done:
-
-        1. Improved parser and error handling.
-
-        3. Implemented a simple BFS proof search. This stores the steps and
-        also finds the *shortest* proof, if one exists. There's also a proof
-        printer now.
 -}
 
 handleStatements :: [Term] -> IO ()
