c962551b460a6c8282dabedda9ca074452bdd834
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-12-12 20:04:10 -0500

    Remove TODO comment

--- main.hs
+++ main.hs
@@ -12,20 +12,6 @@ isQuery :: Term -> Bool
 isQuery (Query _) = True
 isQuery _ = False
 
-{-|
-    Okay, things look pretty good so far!
-
-    Next up:
-
-        Improve the data structure design. I have plans to implement a few
-        different proof algorithms, and the current design is awful. There are
-        `DisList`s and `CNF`s floating around everywhere.
-
-        I think that some of this could be cleared up by using `newtype` rather
-        than `type` to define the `DisList` and `CNF` types, but I might have
-        to experiment a bit and find out.
--}
-
 handleStatements :: [Term] -> IO ()
 handleStatements statements = do
     let (queries, clauses) = partition isQuery statements
